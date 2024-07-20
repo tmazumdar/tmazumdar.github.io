a0e164d65eb79e1e8fabf5a64faf5fbb65528fc7
Tahsin Mazumdar <tahsin.mazumdar@gmail.com>
2024-07-20 15:13:07 -0400

    host img separately

--- src/components/LeftSider.jsx
+++ src/components/LeftSider.jsx
@@ -39,7 +39,7 @@ function LeftSider() {
                 I'm Tahsin
             </Text>
             <br />
-            <Avatar src="assets/tm-avatar.png" shape="square" size={64} />
+            <Avatar src="https://i.postimg.cc/63ypXWKp/tm-avatar.png" shape="square" size={64} />
             <br />
             <Text
                 strong
